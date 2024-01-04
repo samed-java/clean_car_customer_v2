85fbd3d2ab6a9ecc00dd725b972e509e5e23dcd3
samed-java <85803340+samed-java@users.noreply.github.com>
2024-01-04 16:39:32 +0400

    uygeiu

--- lib/features/profile_section/reservation_details/widgets/reservation_detail_content.dart
+++ lib/features/profile_section/reservation_details/widgets/reservation_detail_content.dart
@@ -5,19 +5,21 @@ import 'package:clean_car_customer_v2/utils/pager/pager.dart';
 import 'package:clean_car_customer_v2/utils/pager/transition.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 
 import '../../../branches_and_reservation/reservation/data/model/res/reservation_parameters_res_model.dart';
 import '../../my_cars/data/model/res/my_cars_res_model.dart';
 
 class ReservationDetailContent extends StatelessWidget {
-  const ReservationDetailContent({super.key, required this.isNew,
-  required this.branch,
-  required this.car,
-  required this.service,
-  required this.date,
-  required this.time,
-    this.onSubmit
-  });
+  const ReservationDetailContent(
+      {super.key,
+      required this.isNew,
+      required this.branch,
+      required this.car,
+      required this.service,
+      required this.date,
+      required this.time,
+      this.onSubmit});
   final bool isNew;
   final Branch branch;
   final Car car;
@@ -25,7 +27,6 @@ class ReservationDetailContent extends StatelessWidget {
   final DateTime date;
   final Time time;
   final Function? onSubmit;
-
 
   @override
   Widget build(BuildContext context) {
@@ -39,20 +40,22 @@ class ReservationDetailContent extends StatelessWidget {
               children: [
                 SvgPicture.asset(IconAssets.location),
                 Gaps.w4,
-                Text(
-                  "Nizami filialı, Hadi Rəcəbli küç 123",
+                Expanded(
+                    child: Text(
+                  "${branch.washingName} ${branch.address}",
+                  overflow: TextOverflow.ellipsis,
                   style: getMediumStyle(
                     color: ColorManager.secondaryBlack,
                     fontSize: 14,
                   ).copyWith(decoration: TextDecoration.underline),
-                ),
+                )),
               ],
             ),
           ),
           Gaps.h16,
           ReservationDetailCard(
             content: Text(
-              "KIA Renta, 03-HJ-490 ",
+              "${car.carModel}, ${car.carNumber} ",
               style: getMediumStyle(
                 color: ColorManager.secondaryBlack,
                 fontSize: 14,
@@ -63,7 +66,7 @@ class ReservationDetailContent extends StatelessWidget {
           Gaps.h16,
           ReservationDetailCard(
             content: Text(
-              "Kimyəvi təmizləmə",
+              "${service.title}",
               style: getMediumStyle(
                 color: ColorManager.secondaryBlack,
                 fontSize: 14,
@@ -74,7 +77,7 @@ class ReservationDetailContent extends StatelessWidget {
           Gaps.h16,
           ReservationDetailCard(
             content: Text(
-              "24 sentyabr, 16:30",
+              "${DateFormat("dd.MM.yyyy").format(date)}  ${time.time}",
               style: getMediumStyle(
                 color: ColorManager.secondaryBlack,
                 fontSize: 14,
@@ -83,20 +86,20 @@ class ReservationDetailContent extends StatelessWidget {
             headerText: "Tarix və saat",
           ),
           Gaps.h16,
-          ReservationDetailCard(
-            content: Text(
-              "Kard",
-              style: getMediumStyle(
-                color: ColorManager.secondaryBlack,
-                fontSize: 14,
-              ),
-            ),
-            headerText: "Ödəniş növü",
-          ),
+          // ReservationDetailCard(
+          //   content: Text(
+          //     "Kard",
+          //     style: getMediumStyle(
+          //       color: ColorManager.secondaryBlack,
+          //       fontSize: 14,
+          //     ),
+          //   ),
+          //   headerText: "Ödəniş növü",
+          // ),
           Gaps.h16,
           ReservationDetailCard(
             content: Text(
-              "18 AZN",
+              "${service.price} AZN",
               style: getMediumStyle(
                 color: ColorManager.secondaryBlack,
                 fontSize: 14,
